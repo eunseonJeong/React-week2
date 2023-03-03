e49633bb321e7abbba6d2a12b438c5f7f51723c6
eunseon Jeong <suni980215@gmail.com>
2023-03-03 17:20:32 +0900

    useRef(포커싱 바꾸기)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React,{useRef} from 'react'
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
   
-  const ref = useRef('초기값');
-  console.log('ref',ref)
+  const idRef = useRef("");
+  const pwRef = useRef("");
 
-  ref.current = '변경값'
-  console.log('ref2',ref);
+  const [id,setId] = useState('');
+
+  // 렌더링이 될 때
+  useEffect(() => {
+    idRef.current.focus();
+  }, []);
+
+  useEffect(()=>{
+    if(id.length >= 10) {
+      pwRef.current.focus();
+    }
+  },[id])
 
   return (
-    <div>App</div>
-  )
+    <>
+      <div>
+        아이디 : <input 
+        value={id} onChange={e=>{
+          setId(e.target.value)
+        }}
+        type="text" ref={idRef} />
+      </div>
+
+      <div>
+        비밀번호 : <input type="password" ref={pwRef}  />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
