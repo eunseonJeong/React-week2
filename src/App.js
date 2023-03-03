4feab849b9b3175311480e658c80eb94b2cdb3bf
eunseon Jeong <suni980215@gmail.com>
2023-03-03 22:09:52 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
-import { useEffect, useRef, useState } from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react'
 
 function App() {
-  
-  const idRef = useRef("");
-  const pwRef = useRef("");
 
-  const [id,setId] = useState('');
+  const [count,setCount] = useState(0);
+  const [name,setName] = useState('');
 
-  // 렌더링이 될 때
-  useEffect(() => {
-    idRef.current.focus();
-  }, []);
+  const pulsBtnHandler = () =>{
+    setCount(count + 1)
+  }
+//렌더링 될 떄마다 실행됨
+  // useEffect(() =>{
+  //   console.log('안녕');
+  // },[count])
+
+  // useEffect(() =>{
+  //   console.log('이름');
+  // },[name])
 
   useEffect(()=>{
-    if(id.length >= 10) {
-      pwRef.current.focus();
-    }
-  },[id])
+    console.log('hihi');
+  },[])
+
+  const chgBtnHandelr = (e) =>{
+    setName(e.target.value)
+  }
 
   return (
-    <>
-      <div>
-        아이디 : <input 
-        value={id} onChange={e=>{
-          setId(e.target.value)
-        }}
-        type="text" ref={idRef} />
-      </div>
-
-      <div>
-        비밀번호 : <input type="password" ref={pwRef}  />
-      </div>
-    </>
-  );
+    <div>
+      버튼 : {count}
+      <button onClick={pulsBtnHandler}>플러스</button> <br />
+      <input type='text' value={name} onChange={chgBtnHandelr}/>
+      <span> </span>
+    </div>
+  )
 }
 
-export default App;
+export default App
